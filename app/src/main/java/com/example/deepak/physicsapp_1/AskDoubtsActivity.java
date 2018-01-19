28d0628abb1c3659a653c252eaa1bd4c639ec4e7
dbhardwaj86 <dbhardwaj86@gmail.com>
2018-01-19 17:03:05 +0530

    Added AskDoubtsActivity and wired a few Imagebuttons

--- app/src/main/java/com/example/deepak/physicsapp_1/AskDoubtsActivity.java
+++ app/src/main/java/com/example/deepak/physicsapp_1/AskDoubtsActivity.java
@@ -3,8 +3,15 @@ package com.example.deepak.physicsapp_1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class AskDoubtsActivity extends AppCompatActivity {
+
+    private ImageButton mAddDoubt;
+    private ImageButton mLeftDoubt;
+    private ImageButton mRightDoubt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +19,31 @@ public class AskDoubtsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask_doubts);
         Log.d("PApp", "Ask Doubts activity started");
+
+        mAddDoubt = (ImageButton)findViewById(R.id.add_button);
+        mLeftDoubt = (ImageButton) findViewById(R.id.left_doubt);
+        mRightDoubt = (ImageButton) findViewById(R.id.right_doubt);
+
+        mAddDoubt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("PApp","Add Doubt clicked");
+            }
+        });
+        mLeftDoubt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("PApp","Left Doubt clicked");
+            }
+        });
+        mRightDoubt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("PApp","Right Doubt clicked");
+            }
+        });
+
+
 
     }
 }
