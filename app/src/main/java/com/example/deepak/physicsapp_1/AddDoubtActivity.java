1edd4d3f1b460edf3f2d8f93d940ba5680b169e7
dbhardwaj86 <dbhardwaj86@gmail.com>
2018-01-20 16:14:50 +0530

    Downloading file from FIREBASE

--- app/src/main/java/com/example/deepak/physicsapp_1/AddDoubtActivity.java
+++ app/src/main/java/com/example/deepak/physicsapp_1/AddDoubtActivity.java
@@ -1,5 +1,8 @@
 package com.example.deepak.physicsapp_1;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,25 +11,62 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 public class AddDoubtActivity extends AppCompatActivity {
 
     private EditText mEditText;
     private DatabaseReference mDatabaseReference;
     private RecyclerView mRecyclerView;
-
+    private ImageView mImageView;
+    FirebaseStorage mStorage;
+    //StorageReference storageRef = storage.getReferenceFromUrl("gs://physicsapp1-7f596.appspot.com").child("icons8-more-96.png");
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_doubt);
+        mStorage = FirebaseStorage.getInstance();
+        StorageReference storageRef = mStorage.getReferenceFromUrl("gs://physicsapp1-7f596.appspot.com").child("icons8-more-96.png");
+        mImageView = (ImageView) findViewById(R.id.image);
+        
+        try {
+            final File localFile = File.createTempFile("image", "jpg");
+            Log.d("PApp", "Local file created");
+            storageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    Log.d("PApp", localFile.getAbsolutePath());
+
+                    Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                    if(bitmap != null)
+                        mImageView.setImageBitmap(bitmap);
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                }
+            });
+        } catch (IOException e ) {}
+
+
+
 
         mEditText = (EditText)findViewById(R.id.editText);
         mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("message");
